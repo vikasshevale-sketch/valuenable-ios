d3ddbbe5dcef06596c2c09245a78f239b5bbe259
vikasshevale-sketch <vikas.shevale@valuenable.in>
2026-08-30 10:14:09 +0530

    Update SecurePreviewController.swift

--- DocumentViewer/SecurePreviewController.swift
+++ DocumentViewer/SecurePreviewController.swift
@@ -1,76 +1,50 @@
 import UIKit
 import QuickLook
 
-/// Sandboxed document viewer that suppresses all iOS Share Sheets, AirDrop, Save to Files, and Print actions.
-public final class SecurePreviewController: QLPreviewController, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
-    
-    private let fileURL: URL
-    
-    public init(fileURL: URL) {
+class SecurePreviewController: UIViewController, QLPreviewControllerDataSource, QLPreviewControllerDelegate {
+    private var fileURL: URL?
+
+    init(fileURL: URL) {
         self.fileURL = fileURL
         super.init(nibName: nil, bundle: nil)
-        self.dataSource = self
-        self.delegate = self
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    public override func viewDidLoad() {
+
+    override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = fileURL.lastPathComponent
+        setupPreview()
+    }
+
+    private func setupPreview() {
+        let previewVC = QLPreviewController()
+        previewVC.dataSource = self
+        previewVC.delegate = self
         
-        // Add secure container wrapping to prevent screenshot/screen capture of documents
-        let secureView = SecureContainerView(contentView: self.view)
-        self.view.addSubview(secureView)
-        secureView.pinToSuperview()
-    }
-    
-    public override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        stripExportActions()
-    }
-    
-    public override func viewDidLayoutSubviews() {
-        super.viewDidLayoutSubviews()
-        stripExportActions()
-    }
-    
-    /// Recursively strips iOS Share button (UIActivityViewController / UIBarButtonSystemItemAction)
-    private func stripExportActions() {
+        // Embed QuickLook viewer directly into secure controller
+        addChild(previewVC)
+        view.addSubview(previewVC.view)
+        previewVC.view.frame = view.bounds
+        previewVC.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        previewVC.didMove(toParent: self)
+        
+        // Remove export / action buttons from navigation bar
         navigationItem.rightBarButtonItems = nil
-        navigationItem.rightBarButtonItem = nil
-        navigationItem.leftBarButtonItems = nil
-        
-        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(dismissViewer))
-        navigationItem.leftBarButtonItem = doneButton
-        
-        // Remove action buttons in child navigation controllers
-        if let navBar = navigationController?.navigationBar {
-            navBar.topItem?.rightBarButtonItem = nil
-            navBar.topItem?.rightBarButtonItems = nil
-        }
     }
-    
-    @objc private func dismissViewer() {
-        dismiss(animated: true) {
-            // Securely purge temporary attachment from disk upon closing
-            try? FileManager.default.removeItem(at: self.fileURL)
-        }
-    }
-    
+
     // MARK: - QLPreviewControllerDataSource
-    public func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
-        return 1
+    func numberOfPreviewItems(in controller: QLPreviewController) -> Int {
+        return fileURL != nil ? 1 : 0
     }
-    
-    public func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
-        return fileURL as QLPreviewItem
+
+    func previewController(_ controller: QLPreviewController, previewItemAt index: Int) -> QLPreviewItem {
+        return (fileURL as QLPreviewItem?) ?? NSURL()
     }
-    
-    // MARK: - QLPreviewControllerDelegate (Disable external app handoff)
-    public func previewController(_ controller: QLPreviewController, shouldOpen url: URL, for item: QLPreviewItem) -> Bool {
-        return false // Block external link opening from within documents
+
+    // Disable system activity / sharing options inside QuickLook
+    func previewController(_ controller: QLPreviewController, transitionViewFor item: QLPreviewItem) -> UIView? {
+        return nil
     }
 }
